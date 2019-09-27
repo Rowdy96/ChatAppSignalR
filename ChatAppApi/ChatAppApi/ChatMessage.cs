6ba30b6612e9a8049080e6177632c2dd712d3c9b
Rahul Mondal <RahulMondal@promact.com>
2019-09-27 17:48:02 +0530

    added

---
+++ ChatAppApi/ChatAppApi/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatAppApi
+{
+    public class ChatMessage
+    {
+        public string Text { get; set; }
+        public string SenderName { get; set; }
+        public DateTimeOffset SendAt { get; set; }
+    }
+}
